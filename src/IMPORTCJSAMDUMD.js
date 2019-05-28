1c8d4866e590ed6100c610dd695484e2dcad6cb8
masx200 <34191203+masx200@users.noreply.github.com>
2019-05-28 08:40:21 +0800

    Update IMPORTCJSAMDUMD.js

--- src/IMPORTCJSAMDUMD.js
+++ src/IMPORTCJSAMDUMD.js
@@ -5,7 +5,65 @@
 /**
  *动态异步加载commonjs和umd和amd模块
  *
- *
+ *动态异步加载 commonjs 和 umd 和 amd 模块 ,包装 cjs 和 amd 和 umd 模块为异步加载 promise 方法,使用 fetch 方法来获取指定的模块包源代码 ， 可以把模块放入模块仓库中，
+
+并且解决了包之间的依赖关系，如果依赖包当中的 require 函数需要，则在模块仓库中引入需要的模块包
+
+定义了模块包的 define 方法，require 方法，module.exports 对象和 exports 对象
+
+加载有依赖关系的模块包的方法
+
+(async () => {
+
+  const [jquery, popper] = await Promise.all([
+
+    IMPORTCJSAMDUMD("https://cdn.bootcss.com/jquery/3.4.1/jquery.js", "jquery"),
+
+    IMPORTCJSAMDUMD(
+
+      "https://cdn.staticfile.org/popper.js/1.15.0/umd/popper.min.js",
+
+      "popper.js"
+
+    )
+
+  ]);
+
+  const bootstrap = await IMPORTCJSAMDUMD(
+
+    "https://cdn.staticfile.org/twitter-bootstrap/4.3.1/js/bootstrap.js",
+
+    "bootstrap"
+
+  );
+
+  console.log(jquery, popper, bootstrap);
+
+})();
+
+如果要动态异步加载 es6 模块,加载 es6 模块的方法:
+
+import(url).then(console.log);
+
+动态异步加载 cjs,amd,umd 模块用法:
+
+主函数,IMPORTCJSAMDUMD,返回一个 promise 对象,参数 url 和 name 都是字符串，把通过 url 加载的依赖包放入模块仓库中，命名为 name，promise 之后的.then 函数的回调函数的参数是 module，module.default 的模块的默认输出
+
+IMPORTCJSAMDUMD(url, name);
+
+IMPORTCJSAMDUMD(url).then(m => console.log(m.default));
+
+次对象,IMPORTCJSAMDUMD.GLOBALPACKAGESTORE,是所有加载过的模块的存储仓库对象
+
+IMPORTCJSAMDUMD.GLOBALPACKAGESTORE[name];
+
+次函数,IMPORTCJSAMDUMD.REQUIREPACKAGE,返回模块仓库中的模块,参数 name 是字符串
+
+IMPORTCJSAMDUMD.REQUIREPACKAGE(name);
+
+IMPORTCJSAMDUMD.GLOBALPACKAGESTORE[name]和 IMPORTCJSAMDUMD.REQUIREPACKAGE(name)是相等的
+
+示例
  *
  * 用法:
  *
