f2241d1d338e2a9fea521bf4033b63c841f19a54
masx200 <34191203+masx200@users.noreply.github.com>
2019-08-06 20:18:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import IMPORTCJSAMDUMD from "./IMPORTCJSAMDUMD.js";
 export default IMPORTCJSAMDUMD;
-import dynamicimportshim from "./dynamicimportshim.js";
-export{dynamicimportshim}
+//import dynamicimportshim from "./dynamicimportshim.js";
+//export{dynamicimportshim}
