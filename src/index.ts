f407331411faa74bc966b9f1c4bea87f8c557be7
masx200 <34191203+masx200@users.noreply.github.com>
2020-01-20 09:16:12 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import importcjsamdumd, {
   PACKAGESTORE,
   REQUIREPACKAGE
 } from "./importcjsamdumd.js";
-export default importcjsamdumd;
+//export default importcjsamdumd;
 export { PACKAGESTORE, REQUIREPACKAGE, importcjsamdumd };
 import { packagealias } from "./alias";
 export { packagealias };
