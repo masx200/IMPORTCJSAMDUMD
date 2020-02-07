a4e656e135c01340c46184280005ca9236de8393
masx200 <34191203+masx200@users.noreply.github.com>
2020-02-07 21:48:07 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,21 @@
  import babel from 'rollup-plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
 import sourcemaps from 'rollup-plugin-sourcemaps';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
 import typescript from 'rollup-plugin-ts';
-const banner = `const globalThis = Function('return this')();
-const self = globalThis;
-const window = globalThis;
-const global = globalThis;
-const {WeakSet,WeakMap,Date, RegExp, Event, CustomEvent, requestAnimationFrame, URL, Blob, Element, Node, String, Array, document, Object, Reflect, Proxy, Symbol, Boolean, Promise, Set, Math, Error, TypeError, JSON, Map, clearTimeout, setTimeout, parseInt,Number} = globalThis;`;
 const beautifyterserplugin = terser({
-    sourcemap: true,
     compress: false,
     mangle: false,
     output: {
-        ascii_only: !0,
         comments: !1,
         beautify: true
     }
 });
 const mybabelplugin = babel({
+    sourcemap: true,
+    plugins: ['@babel/plugin-proposal-optional-catch-binding'],
     presets: [[
             '@babel/preset-env',
             {
@@ -31,14 +26,7 @@ const mybabelplugin = babel({
                     'last 1 firefox version'
                 ]
             }
-        ]],
-    plugins: [
-        '@babel/plugin-proposal-optional-catch-binding',
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-syntax-nullish-coalescing-operator',
-        '@babel/plugin-transform-typescript',
-        '@babel/plugin-proposal-nullish-coalescing-operator'
-    ]
+        ]]
 });
 const myterserplugin = terser({
     sourcemap: true,
@@ -58,37 +46,34 @@ const myterserplugin = terser({
 });
 export default [
     {
-        input: './index.ts',
+        input: './src/index.ts',
         output: [{
-                sourceMap: true,
-                banner,
                 file: './dist/index.js',
                 format: 'esm',
                 sourcemap: true
             }],
         plugins: [
-            typescript(),
+            sourcemaps(),
             json(),
             resolve(),
             commonjs(),
-            sourcemaps(),
+            typescript({}),
             beautifyterserplugin
         ]
     },
     {
         input: './dist/index.js',
         output: [{
-                sourceMap: true,
                 file: './dist/index.js',
                 format: 'esm',
                 sourcemap: true
             }],
         plugins: [
             mybabelplugin,
+            sourcemaps(),
             json(),
             resolve(),
             commonjs(),
-            sourcemaps(),
             beautifyterserplugin
         ]
     },
@@ -100,26 +85,12 @@ export default [
                 sourcemap: true
             }],
         plugins: [
+            sourcemaps(),
             mybabelplugin,
             resolve(),
             commonjs(),
-            myterserplugin,
-            sourcemaps()
-        ]
-    },
-    {
-        input: './polyfill/index.js',
-        output: [{
-                file: './dist/polyfill.js',
-                format: 'iife',
-                sourcemap: true
-            }],
-        plugins: [
-            mybabelplugin,
-            resolve(),
-            commonjs(),
-            myterserplugin,
-            sourcemaps()
+            json(),
+            myterserplugin
         ]
     }
 ];
