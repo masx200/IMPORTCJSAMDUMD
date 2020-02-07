4839aef9ba3f69aca3537427a69d13e0542ea0b9
masx200 <34191203+masx200@users.noreply.github.com>
2020-02-07 21:45:37 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,114 +1,125 @@
-import babel from "rollup-plugin-babel";
-import sourcemaps from "rollup-plugin-sourcemaps";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-import json from "@rollup/plugin-json";
-import typescript from "rollup-plugin-ts";
+ import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-ts';
+const banner = `const globalThis = Function('return this')();
+const self = globalThis;
+const window = globalThis;
+const global = globalThis;
+const {WeakSet,WeakMap,Date, RegExp, Event, CustomEvent, requestAnimationFrame, URL, Blob, Element, Node, String, Array, document, Object, Reflect, Proxy, Symbol, Boolean, Promise, Set, Math, Error, TypeError, JSON, Map, clearTimeout, setTimeout, parseInt,Number} = globalThis;`;
 const beautifyterserplugin = terser({
-  compress: false,
-  mangle: false,
-  output: {
-    comments: !1,
-    beautify: true
-  }
+    sourcemap: true,
+    compress: false,
+    mangle: false,
+    output: {
+        ascii_only: !0,
+        comments: !1,
+        beautify: true
+    }
 });
 const mybabelplugin = babel({
-  sourcemap: true,
-
-  //extensions: [".jsx", ".ts", ".js", ".tsx"],
-
-  plugins: ["@babel/plugin-proposal-optional-catch-binding"],
-  presets: [
-    //"@babel/preset-typescript",
-    [
-      "@babel/preset-env",
-      {
-        targets: [
-          "last 1 edge version",
-          "last 1 safari version",
-          "last 1 chrome version",
-          "last 1 firefox version"
-        ]
-      }
+    presets: [[
+            '@babel/preset-env',
+            {
+                targets: [
+                    'last 1 edge version',
+                    'last 1 safari version',
+                    'last 1 chrome version',
+                    'last 1 firefox version'
+                ]
+            }
+        ]],
+    plugins: [
+        '@babel/plugin-proposal-optional-catch-binding',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-syntax-nullish-coalescing-operator',
+        '@babel/plugin-transform-typescript',
+        '@babel/plugin-proposal-nullish-coalescing-operator'
     ]
-  ]
 });
 const myterserplugin = terser({
-  sourcemap: true,
-  toplevel: true,
-  output: {
-    comments: !1,
-    ascii_only: !0
-  },
-  compress: {
+    sourcemap: true,
     toplevel: true,
-    unused: true,
-    drop_console: true,
-    drop_debugger: true,
-    pure_funcs: ["console.log"]
-  },
-  mangle: { properties: false }
+    output: {
+        comments: !1,
+        ascii_only: !0
+    },
+    compress: {
+        toplevel: true,
+        unused: true,
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log']
+    },
+    mangle: { properties: false }
 });
 export default [
-  {
-    input: "./src/index.ts",
-    output: [
-      {
-        file: "./dist/index.js",
-        format: "esm",
-        sourcemap: true
-      }
-    ],
-    plugins: [
-      //mybabelplugin,
-      sourcemaps(),
-      json(),
-      resolve(),
-      commonjs(),
-      typescript({}),
-
-      beautifyterserplugin
-    ]
-  },
-  {
-    input: "./dist/index.js",
-    output: [
-      {
-        file: "./dist/index.js",
-        format: "esm",
-        sourcemap: true
-      }
-    ],
-    plugins: [
-      mybabelplugin,
-      sourcemaps(),
-      json(),
-      resolve(),
-      commonjs(),
-      //    typescript({}),
-
-      beautifyterserplugin
-    ]
-  },
-
-  {
-    input: "./dist/index.js",
-    output: [
-      {
-        file: "./dist/index.min.js",
-        format: "esm",
-        sourcemap: true
-      }
-    ],
-    plugins: [
-      sourcemaps(),
-      mybabelplugin,
-      resolve(),
-      commonjs(),
-
-      json(),
-      myterserplugin
-    ]
-  }
+    {
+        input: './index.ts',
+        output: [{
+                sourceMap: true,
+                banner,
+                file: './dist/index.js',
+                format: 'esm',
+                sourcemap: true
+            }],
+        plugins: [
+            typescript(),
+            json(),
+            resolve(),
+            commonjs(),
+            sourcemaps(),
+            beautifyterserplugin
+        ]
+    },
+    {
+        input: './dist/index.js',
+        output: [{
+                sourceMap: true,
+                file: './dist/index.js',
+                format: 'esm',
+                sourcemap: true
+            }],
+        plugins: [
+            mybabelplugin,
+            json(),
+            resolve(),
+            commonjs(),
+            sourcemaps(),
+            beautifyterserplugin
+        ]
+    },
+    {
+        input: './dist/index.js',
+        output: [{
+                file: './dist/index.min.js',
+                format: 'esm',
+                sourcemap: true
+            }],
+        plugins: [
+            mybabelplugin,
+            resolve(),
+            commonjs(),
+            myterserplugin,
+            sourcemaps()
+        ]
+    },
+    {
+        input: './polyfill/index.js',
+        output: [{
+                file: './dist/polyfill.js',
+                format: 'iife',
+                sourcemap: true
+            }],
+        plugins: [
+            mybabelplugin,
+            resolve(),
+            commonjs(),
+            myterserplugin,
+            sourcemaps()
+        ]
+    }
 ];
