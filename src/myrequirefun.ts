b51dc7a70bb74bb0f8db552dfd841de266688288
masx200 <34191203+masx200@users.noreply.github.com>
2020-02-07 17:14:28 +0800

    Update myrequirefun.ts

--- src/myrequirefun.ts
+++ src/myrequirefun.ts
@@ -1,13 +1,13 @@
 import { assertstring } from "./assertstring";
 import { cantfindError } from "./cantfindError";
-import { PACKAGESTORE, 模块仓库中没有找到 } from "./importcjsamdumd";
+import { packagestore, 模块仓库中没有找到 } from "./importcjsamdumd";
 import { packagealias } from "./alias";
 
 export const myrequirefun = function requireinstead(packagename: string) {
   assertstring(packagename);
 
   const findpackage =
-    PACKAGESTORE[packagename] || PACKAGESTORE[packagealias[packagename]];
+    packagestore[packagename] || packagestore[packagealias[packagename]];
   if (findpackage) {
     Object.freeze(findpackage);
     return findpackage.default ?? findpackage;
