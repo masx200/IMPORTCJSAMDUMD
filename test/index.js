7dcee6697451a234a37601c88c2c28c4d22dc3fb
masx200 <34191203+masx200@users.noreply.github.com>
2020-02-18 13:29:38 +0800

    logger解决循环引用json stringify问题

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 import logger from "./logger.js";
-(console.log = logger), console.log("importcjsamdumd test");
+logger("importcjsamdumd test");
 import * as cjsamdumd from "../dist/index.min.js";
 import {
     getmoduletype,
@@ -24,83 +24,83 @@ Object.assign(packagealias, {
         "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io/src/assetsjs/md5.js"
 });
 const allpro = Promise.all([
-    importcjsamdumd("./array.json").then(console.log),
-    importcjsamdumd(["./es1.js", "./es2.js"]).then(console.log),
+    importcjsamdumd("./array.json").then(logger),
+    importcjsamdumd(["./es1.js", "./es2.js"]).then(logger),
     importcjsamdumd(["./amd1.js", "./cjs1.js", "./cmd1.js", "./cmd2.js"]).then(
-        console.log
+        logger
     ),
-    importcjsamdumd(["./amd2.js", "./cjs2.js"]).then(console.log),
+    importcjsamdumd(["./amd2.js", "./cjs2.js"]).then(logger),
     importcjsamdumd("https://cdn.staticfile.org/vue/2.6.10/vue.esm.browser.js")
-        .then(console.log)
+        .then(logger)
         .then(() => {
-            console.log("vue", requirepackage("vue"));
+            logger("vue", requirepackage("vue"));
         }),
     importcjsamdumd(
         "https://cdn.staticfile.org/react/16.10.2/umd/react.production.min.js",
         "react"
     )
-        .then(console.log)
+        .then(logger)
         .then(() => {
-            console.log("react", requirepackage("react"));
+            logger("react", requirepackage("react"));
         }),
     importcjsamdumd(
         "https://cdn.jsdelivr.net/npm/fast-xml-parser@3.14.0/src/parser.js"
-    ).then(console.log),
+    ).then(logger),
     importcjsamdumd(
         "https://cdn.jsdelivr.net/npm/jquery@3.2.1/package.json"
-    ).then(console.log),
+    ).then(logger),
     importcjsamdumd(["bootstrap", "jquery"])
-        .then(console.log)
+        .then(logger)
         .then(() => {
-            console.log("jquery", requirepackage("jquery")),
-                console.log("bootstrap", requirepackage("bootstrap"));
+            logger("jquery", requirepackage("jquery")),
+                logger("bootstrap", requirepackage("bootstrap"));
         }),
     importcjsamdumd(
         "https://cdn.jsdelivr.net/npm/highlight.js@9.15.10/lib/highlight.js",
         "hljs"
     )
-        .then(console.log)
+        .then(logger)
         .then(() => {
-            console.log("hljs", requirepackage("hljs"));
+            logger("hljs", requirepackage("hljs"));
         }),
     importcjsamdumd(
         "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io/src/assetsjs/md5.js",
         "md5"
     )
-        .then(console.log)
+        .then(logger)
         .then(() => {
-            console.log("md5", requirepackage("md5"));
+            logger("md5", requirepackage("md5"));
         }),
     importcjsamdumd([
         "https://cdn.staticfile.org/decimal.js/10.2.0/decimal.js",
         "https://cdn.staticfile.org/big-integer/1.6.47/BigInteger.js",
         "https://cdn.staticfile.org/lodash.js/4.17.15/lodash.js"
-    ]).then(console.log),
+    ]).then(logger),
     importcjsamdumd([
         "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.4.1/src/assetsjs/hieroglyphy.js",
         "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.4.1/src/assetsjs/jsfuck.js",
         "https://cdn.jsdelivr.net/gh/masx200/masx200.github.io@4.4.1/src/assetsjs/fast-xml-parser.js"
-    ]).then(console.log),
+    ]).then(logger),
     importcjsamdumd("https://unpkg.com/core-js@3.6.4/modules/es.map.js").then(
-        console.log
+        logger
     )
 ]);
-console.log(Object.assign({}, cjsamdumd));
+logger(Object.assign({}, cjsamdumd));
 allpro.then(() => {
     const allids = getmoduleids();
-    console.log(allids);
+    logger(allids);
 
-    console.log(
+    logger(
         allids.map(id => {
             return [id, getmoduledeps(id)];
         })
     );
-    console.log(
+    logger(
         allids.map(id => {
             return [id, getmodulesource(id)];
         })
     );
-    console.log(
+    logger(
         allids.map(id => {
             return [id, getmodulewrapper(id)];
         })
@@ -109,19 +109,20 @@ allpro.then(() => {
     console.info(allmodule);
     allmodule.forEach(v => {
         try {
-            console.log(v);
+            logger(v);
         } catch (error) {
             console.error(error);
         }
     });
-    console.log(
+    logger(
         allids.map(id => {
             return [id, getmoduletype(id)];
         })
     );
 });
 window.addEventListener("unhandledrejection", e => {
-    throw e;
+    // throw e;
+    console.error(e);
 });
 window.addEventListener("error", e => {
     console.error(e);
--- test/logger.js
+++ test/logger.js
@@ -1,20 +1,46 @@
 const log = console.log;
+// let count = 0;
+function anticircle() {
+    const objset = new WeakSet();
+    /**
+     * @param {string} k
+     * @param {any} a
+     */
+    const replacer = (k, a) => {
+        // count++;
+        // console.info(count);
+        if ((a && typeof a === "object") || typeof a === "function") {
+            if (objset.has(a)) {
+                const tag=Object.prototype.toString.call(a)
+                return "[object circular]"+" "+tag;
+            } else {
+                objset.add(a);
+            }
+        }
+
+        if (typeof a === "function") {
+            let keys = Object.keys(a);
+            if (keys.length) {
+                return Object.assign({}, a);
+            } else {
+                return `function ${a.name || k}(){} `;
+            }
+        } else {
+            return a;
+        }
+    };
+    return replacer;
+}
+
 /**
  * @param {any[]} args
  */
-export default function(...args) {
+export default async function(...args) {
     log(...args);
-
-    let p = document.createElement("p");
-    p.innerText = JSON.stringify(
-        args,
-        (k, a) => {
-            if (typeof a === "function") return `function ${a.name || k}(){}`;
-            else return a;
-        },
-        4
-    );
-    document.body.appendChild(p);
-    let hr = document.createElement("hr");
-    document.body.appendChild(hr);
+    log(JSON.stringify(args, anticircle(), 4));
+    // let p = document.createElement("p");
+    // p.innerText = JSON.stringify(args, anticircle(), 4);
+    // document.body.appendChild(p);
+    // let hr = document.createElement("hr");
+    // document.body.appendChild(hr);
 }
