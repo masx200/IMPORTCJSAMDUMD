bbb346bedf86f21c6589473eef375123461adb8e
masx200 <34191203+masx200@users.noreply.github.com>
2020-03-31 22:34:42 +0800

    修复bug,可以正确使用模块别名了

--- dist/index.js
+++ dist/index.js
@@ -146,6 +146,14 @@ async function 同时发起多个字符串(a, importcjsamdumd) {
 
 const urlsymbol = Symbol("url");
 
+function assert_url(url) {
+    try {
+        url = new URL(url).href;
+    } catch (_unused) {
+        throw Error("invalid url " + url);
+    }
+}
+
 const 输入的类型错误输入的类型必须是字符串或者数组或对象 = "The type entered is incorrect, the type entered must be a string or an array ";
 
 async function oldimportcjsamdumd(url, packagename) {
@@ -164,26 +172,17 @@ async function oldimportcjsamdumd(url, packagename) {
         }
         return suoyouimportpromise;
     } else if (typeof url === "string") {
+        url = (_a = packagealias[url]) !== null && _a !== void 0 ? _a : url;
         assertstring(url);
-        try {
-            url = new URL(url).href;
-        } catch (_unused) {
-            url = (_a = packagealias[url]) !== null && _a !== void 0 ? _a : url;
-        }
         if (String(url).startsWith("./") || String(url).startsWith("../")) {
             var urlobj = new URL(url, location.href);
             url = urlobj.origin + urlobj.pathname;
         }
-        try {
-            url = new URL(url).href;
-        } catch (_unused2) {
-            throw Error("invalid url " + url);
-        }
-        if (typeof packagename === "undefined") {
-            packagename = new URL(url).href;
-        }
-        if (packagename) {
+        assert_url(url);
+        if (typeof packagename === "string") {
             packagealias[packagename] = url;
+        } else if (typeof packagename === "undefined") {
+            packagename = new URL(url).href;
         }
         if (typeof packagestore[packagename] !== "undefined" && get(packagestore[packagename], urlsymbol) === url) {
             return getmodule(packagename);
